e548923648f33611cb8195dcf448bb9a0050a3ea
Henadzi Shutko <hazardsoft@gmail.com>
2023-11-05 20:11:15 +0300

    feat: add pipeline

--- src/cdk-workshop-pipeline.ts
+++ src/cdk-workshop-pipeline.ts
@@ -1,5 +1,6 @@
 import { Stack } from "aws-cdk-lib";
 import { Repository } from "aws-cdk-lib/aws-codecommit";
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class PipelineStack extends Stack {
@@ -8,6 +9,15 @@ export class PipelineStack extends Stack {
 
         const repo = new Repository(this, "CDKWorkshopRepository", {
             repositoryName: "CDKWorkshopRepository"
+        })
+
+        const pipeline = new CodePipeline(this, "CDKWorkshopPipeline", {
+            pipelineName: "CDKWorkshopPipeline",
+            synth: new CodeBuildStep("Step1", {
+                input: CodePipelineSource.codeCommit(repo, "feature-pipeline"),
+                installCommands: ["npm install -g aws-cdk"],
+                commands: ["npm ci", "npm run build", "npx cdk synth"]
+            })
         })
     }
 }
