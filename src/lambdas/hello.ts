03c154913905f86f19616668f0a8c1b184d2b68a
Henadzi Shutko <hazardsoft@gmail.com>
2024-06-08 09:44:09 +0300

    feat: add eslint/prettier to the project

--- src/lambdas/hello.ts
+++ src/lambdas/hello.ts
@@ -1,11 +1,11 @@
-import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 export const handler = async function (event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
-    console.log("event type:", event.constructor.name);
-    console.log("request:", JSON.stringify(event, undefined, 2));
-    return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Good Night, CDK! You've hit ${event.path}\n`,
-    };
-};
+  console.log('event type:', event.constructor.name)
+  console.log('request:', JSON.stringify(event, undefined, 2))
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: `Good Night, CDK! You've hit ${event.path}\n`
+  }
+}
