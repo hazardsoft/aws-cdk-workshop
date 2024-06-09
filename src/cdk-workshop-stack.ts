7f16693515f0e1efafb7752d6db432ae88561a10
Henadzi Shutko <hazardsoft@users.noreply.github.com>
2024-06-09 10:10:21 +0300

    feat: add eslint/prettier to the project (#11)

--- src/cdk-workshop-stack.ts
+++ src/cdk-workshop-stack.ts
@@ -1,21 +1,21 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
-import { Construct } from "constructs";
-import { HitCounter } from "./constructs/hitcounter.js";
-import { Hello } from "./constructs/hello.js";
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway'
+import { Construct } from 'constructs'
+import { HitCounter } from './constructs/hitcounter.js'
+import { Hello } from './constructs/hello.js'
 
 export class CdkWorkshopStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
-        const hello = new Hello(this, "HelloConstruct");
+    const hello = new Hello(this, 'HelloConstruct')
 
-        const hitCounter = new HitCounter(this, "HitCounterConstruct", {
-            downstream: hello.handler,
-        });
+    const hitCounter = new HitCounter(this, 'HitCounterConstruct', {
+      downstream: hello.handler
+    })
 
-        new LambdaRestApi(this, "Endpoint", {
-            handler: hitCounter.handler,
-        });
-    }
+    new LambdaRestApi(this, 'Endpoint', {
+      handler: hitCounter.handler
+    })
+  }
 }
